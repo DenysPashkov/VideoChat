656b22a665adc188cb22498be1ca2200804098c2
denys <dexixoxa@yahoo.com>
2020-04-04 17:06:26 +0200

    Initial Commit

---
+++ VideoChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoChat
+//
+//  Created by admin on 04/04/2020.
+//  Copyright © 2020 DenysPashkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
